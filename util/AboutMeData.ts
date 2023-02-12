a465049b0117884afc0892cc8bd13bc0b68c3ab6
Armen Merzaian <71707172+armenmerzaian@users.noreply.github.com>
2023-02-12 14:55:14 -0500

    Update AboutMeData.ts

--- util/AboutMeData.ts
+++ util/AboutMeData.ts
@@ -1,7 +1,7 @@
 import { IAboutMe } from "@/interface/IAboutMe";
 
 export const aboutMeData: IAboutMe = {
-    paragraph1: "Hi, I'm Armen Merzaian, a software developer based in Toronto with 3 years of experience in full-stack web and mobile development. My specialty tools are React, TypeScript, C++, and Java, and have I a passion for building software to improve people's lives.",
+    paragraph1: "Hi, I'm Armen Merzaian, a software developer based in Toronto with 3 years of experience in full-stack web and mobile development. My specialty tools are React, TypeScript, C++, and Java, and I have a passion for building software to improve people's lives.",
     paragraph2: "I'm an avid learner, so when I'm not working, I enjoy reading non-fiction books and walking, which help me stay physically and mentally fit. I also have an interest in finance and financial technologies, as well as computer vision and AI algorithms, and their potential to change our daily lives.",
     paragraph3: "I believe in giving back to the community and have found fulfillment in tutoring kids in math and mentoring first-year college students in programming.", 
     paragraph4: "I am excited to share my portfolio with you and hope it gives you a glimpse into my skills, experience, and passion. If you have any questions or are interested in working with me, please don't hesitate to reach out. You can also find me on LinkedIn and GitHub and my resume is available for download.",
